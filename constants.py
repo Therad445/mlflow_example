64315776dff9790e886713b61851b9e77b949b4e
Радмир Исламов <therad445@outlook.com>
2026-02-18 20:24:28 +0300

    refactor: use project-relative paths

--- constants.py
+++ constants.py
@@ -1,5 +1,18 @@
-DATASET_PATH_PATTERN = '/app/data/{split_name}.csv'
-DATASET_NAME = 'scikit-learn/adult-census-income'
-MODEL_FILEPATH = '/app/model.joblib'
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parent
+
+DATA_DIR = PROJECT_ROOT / "data"
+PARAMS_DIR = PROJECT_ROOT / "params"
+ARTIFACTS_DIR = PROJECT_ROOT / "artifacts"
+
+DATASET_PATH_PATTERN = str(DATA_DIR / "{split_name}.csv")
+DATASET_NAME = "scikit-learn/adult-census-income"
+
+MODEL_FILEPATH = str(PROJECT_ROOT / "model.joblib")
+
 RANDOM_STATE = 42
 TEST_SIZE = 0.3
+
+MLFLOW_TRACKING_URI = "http://158.160.2.37:5000"
+EXPERIMENT_NAME = "homework_islamov"
